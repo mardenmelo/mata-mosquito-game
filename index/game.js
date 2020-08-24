c17176d499f981a7d1ad06853c08259c744462b2
Marden Melo <mardenmelo@gmail.com>
2020-08-23 21:42:58 -0300

    Game Created

--- index/game.js
+++ index/game.js
@@ -1,5 +1,19 @@
 var altura = 0
 var largura = 0
+var vidas = 1
+var tempo = 15
+var criaMosquitoTempo = 1500
+
+var nivel = window.location.search
+nivel = nivel.replace('?','')
+
+if(nivel === 'normal') {
+    criaMosquitoTempo = 1500
+} else if(nivel === 'dificil') {
+    criaMosquitoTempo = 1000
+} else if(nivel === 'hardcore') {
+    criaMosquitoTempo = 750
+}
 
 function ajustaTamanhoPalco() {
     altura = window.innerHeight
@@ -9,12 +23,30 @@ function ajustaTamanhoPalco() {
 
 ajustaTamanhoPalco()
 
+var cronometro = setInterval(function() {
+    tempo -= 1
+    if(tempo < 0) {
+        clearInterval(cronometro)
+        clearInterval(criaMosquito)
+        window.location.href='vitoria.html'
+        
+    }
+    document.getElementById('cronometro').innerHTML = tempo
+}, 1000)
+
 function posicaoRandom() {
 
     //remove o mosquito anterior caso exista
 
     if(document.getElementById('mosquito')) {
         document.getElementById('mosquito').remove()
+
+        if(vidas > 3) {
+            window.location.href= 'fim_de_jogo.html'
+        } else {
+
+        document.getElementById('v' + vidas).src="imagens/coracao_vazio.png"
+        vidas++ }
     }
 
     var posicaoX = Math.floor(Math.random() * largura) - 90
@@ -33,11 +65,12 @@ function posicaoRandom() {
     mosquito.style.top = posicaoY + "px"
     mosquito.style.position = "absolute"
     mosquito.id = 'mosquito'
+    mosquito.onclick = function() {
+        this.remove()
+    }
     
     document.body.appendChild(mosquito)
-
 }
-
 
 function tamanhoAleatorio() {
     var acesso = Math.floor(Math.random() * 3)
